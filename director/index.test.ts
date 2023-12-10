79e1d2529e0c0faeb3a8935f85c0c9072393656f
Craig Mulligan <git@craigmulligan.com>
2023-12-09 16:48:57 -0800

    Getting there

---
+++ director/index.test.ts
@@ -0,0 +1,64 @@
+import Director from "./";
+import res from "./res.json";
+import assert from "assert/strict";
+import { mock, describe, test, beforeEach } from "node:test";
+
+let d: Director;
+beforeEach(() => {
+  d = new Director(res as any, { location: [-0.052848, 51.53507] });
+});
+
+describe("update Location", () => {
+  test("should emit closest next step", () => {
+    d.on("step", (step) => {
+      assert.strictEqual(step.name, "Virginia Road");
+    });
+    d.updateLocation([-0.073446, 51.528247]);
+  });
+
+  // test("should only be called once when locations are near one another", () => {
+  //   const cb = mock.fn();
+  //   d.on("step", cb);
+  //   d.updateLocation([-0.073446, 51.528247]);
+  //   d.updateLocation([-0.073447, 51.528247]);
+
+  //   assert.strictEqual(cb.mock.callCount(), 1);
+  // });
+
+  // test("should emit deviation event when user leaves route", () => {
+  //   const cb = mock.fn();
+  //   d.on("deviation", cb);
+  //   d.updateLocation([-0.1, 51.528247]);
+
+  //   assert.strictEqual(cb.mock.callCount(), 1);
+  // });
+});
+
+describe.only(".hasDeviated", () => {
+  test("should return true if current location is inside of route buffer", () => {
+    assert.ok(d.hasDeviated());
+  });
+
+  test("should return false if current location is outside of route buffer", () => {
+    d.location = [-0.052848, 52.53507];
+    assert.strictEqual(d.hasDeviated(), false);
+  });
+});
+
+// describe(".nextStep", () => {
+//   test("should return the next closest step (forwards)", () => {
+//     const step = d.nextStep();
+//     assert.strictEqual(step.name, "Regent's Canal towpath");
+//     d.location = [-0.073446, 51.528247];
+//     const step2 = d.nextStep();
+//     assert.strictEqual(step2.name, "Virginia Road");
+//   });
+
+//   test("should return the next step with regards to route bearing", () => {
+//     const step = d.nextStep();
+//     assert.strictEqual(step.name, "Regent's Canal towpath");
+//     d.location = [-0.073446, 51.528247];
+//     const step2 = d.nextStep();
+//     assert.strictEqual(step2.name, "Virginia Road");
+//   });
+// });
--- index.js
+++ index.js
@@ -2,31 +2,32 @@ const fs = require('fs')
 const mbxDirections = require('@mapbox/mapbox-sdk/services/directions')
 
 const directionsClient = mbxDirections({
-  accessToken: null,
+  accessToken,
 })
-;(async () => {
-  const config = {
-    profile: 'cycling',
-    steps: true,
-    voiceUnits: 'metric',
-    geometries: 'geojson',
-    waypoints: [
-      {
-        name: 'Start',
-        coordinates: [-0.052848, 51.53507],
-      },
-      {
-        name: 'End',
-        coordinates: [-0.077387, 51.526756],
-      },
-    ],
-  }
+  ; (async () => {
+    const config = {
+      profile: 'cycling',
+      steps: true,
+      voiceUnits: 'metric',
+      geometries: 'geojson',
+      overview: "false",
+      waypoints: [
+        {
+          name: 'Start',
+          coordinates: [-0.052848, 51.53507],
+        },
+        {
+          name: 'End',
+          coordinates: [-0.077387, 51.526756],
+        },
+      ],
+    }
 
-  const res = await directionsClient
-    .getDirections(config)
-    .send(config)
-    .then(res => {
-      fs.writeFileSync('res.json', JSON.stringify(res.body, null, 2))
-    })
-    .catch(console.log)
-})()
+    const res = await directionsClient
+      .getDirections(config)
+      .send(config)
+      .then(res => {
+        fs.writeFileSync('./director/res.json', JSON.stringify(res.body, null, 2))
+      })
+      .catch(console.log)
+  })()
