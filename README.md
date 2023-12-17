a19d4e4cefb19425a8ab577c3b52d6c403360c1c
Craig Mulligan <git@craigmulligan.com>
2023-12-17 18:43:44 -0500

    update todos

--- README.md
+++ README.md
@@ -1,55 +1,7 @@
 # Experiment on using limited haptic feed to give directions
 
-# Maneuver types
+## TODO
 
-* turn
-  A basic turn in the direction of the modifier.
-
-  * left
-  * right
-
-* new name
-  The road name changes (after a mandatory turn).
-
-* depart
-  Indicates departure from a leg. The modifier value indicates the position of the departure point in relation to the current direction of travel.
-
-* arrive
-  Indicates arrival to a destination of a leg. The modifier value indicates the position of the arrival point in relation to the current direction of travel.
-
-* merge
-  Merge onto a street.
-
-* end of road
-  Road ends in a T intersection.
-
-* continue
-  Continue on a street after a turn.
-
-* rotary
-
-* roundabout turn
-
-* notification
-
-* exit roundabout
-
-* exit rotary
-
-modifiers
-
-* uturn Indicates a reversal of direction
-* sharp right A sharp right turn
-* right A normal turn to the right
-* slight right A slight turn to the right
-* straight No relevant change in direction
-* slight left A slight turn to the left
-* left A normal turn to the left
-* sharp left A sharp turn to the left
-
-We may need a buffer, to call attention to the signal?
-
-TODOs:
-
-* Create an emitter that takes a mapbox direction response and emits manuevers based on current location.
-* Create a fn to translate the emitted manuevers to ios/android vibration api calls based on some mapping.
+1. Handle deviation and re-routing in `./director`
+2. Add basic vibration mapping
+3. Add some live testing infra
--- director/index.test.ts
+++ director/index.test.ts
@@ -11,8 +11,8 @@ beforeEach(() => {
   d = new Director(res as any, { location: [-118.506001, 34.022483] });
 });
 
-describe("simple", () => {
-  test("should emit all steps until done", () => {
+describe("routing", () => {
+  test("should handle simple route", () => {
     const stepCallback = mock.fn();
     const finishCallback = mock.fn();
     d.on("step", stepCallback);
@@ -34,28 +34,9 @@ describe("simple", () => {
     assert.strictEqual(finishCallback.mock.callCount(), 1);
   });
 
-  // test("should only be called once when locations are near one another", () => {
-  //   const cb = mock.fn();
-  //   d.on("step", cb);
-  //   d.updateLocation([-0.073446, 51.528247]);
-  //   d.updateLocation([-0.073447, 51.528247]);
-
-  //   assert.strictEqual(cb.mock.callCount(), 1);
-  // });
-
-  // test("should emit deviation event when user leaves route", () => {
-  //   const cb = mock.fn();
-  //   d.on("deviation", cb);
-  //   d.updateLocation([-0.1, 51.528247]);
-
-  //   assert.strictEqual(cb.mock.callCount(), 1);
-  // });
-});
-
-describe("multi + skips", () => {
-  // this test cases has multipe locations per line segment and
-  // in some cases doesn't have a location within lead distance of a manuever
-  test("multiSkipSteps", () => {
+  test("should handle route with skipped locations", () => {
+    // this test cases has multipe locations per line segment and
+    // in some cases doesn't have a location within lead distance of a manuever
     const stepCallback = mock.fn();
     const finishCallback = mock.fn();
     d.on("step", stepCallback);
@@ -90,42 +71,9 @@ describe("multi + skips", () => {
     // // line segment so we should skip step 1
     d.updateLocation(coords[5]);
     d.updateLocation(coords[6]);
+    d.updateLocation(coords[7]);
     d.updateLocation(coords[8]);
     assert.strictEqual(stepCallback.mock.callCount(), 3);
-    // assert.strictEqual(stepCallback.mock.callCount(), 1);
-    // d.updateLocation(coords[8]);
-    // assert.strictEqual(stepCallback.mock.callCount(), 8);
-    // assert.strictEqual(finishCallback.mock.callCount(), 1);
+    assert.strictEqual(finishCallback.mock.callCount(), 1);
   });
 });
-
-// describe.only(".hasDeviated", () => {
-//   test("should return true if current location is inside of route buffer", () => {
-//     console.log("Heyo");
-//     console.log(JSON.stringify(d.stepRoute));
-//     assert.ok(d.hasDeviated());
-//   });
-
-//   test("should return false if current location is outside of route buffer", () => {
-//     d.location = [-0.052848, 52.53507];
-//     assert.strictEqual(d.hasDeviated(), false);
-//   });
-// });
-
-// describe(".nextStep", () => {
-//   test("should return the next closest step (forwards)", () => {
-//     const step = d.nextStep();
-//     assert.strictEqual(step.name, "Regent's Canal towpath");
-//     d.location = [-0.073446, 51.528247];
-//     const step2 = d.nextStep();
-//     assert.strictEqual(step2.name, "Virginia Road");
-//   });
-
-//   test("should return the next step with regards to route bearing", () => {
-//     const step = d.nextStep();
-//     assert.strictEqual(step.name, "Regent's Canal towpath");
-//     d.location = [-0.073446, 51.528247];
-//     const step2 = d.nextStep();
-//     assert.strictEqual(step2.name, "Virginia Road");
-//   });
-// });
