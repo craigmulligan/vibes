77f5e534225f2cd4d795f896db1b2329ee8750b8
Craig Mulligan <git@craigmulligan.com>
2024-01-14 10:56:50 -0800

    cleanup

---
+++ components/FullScreenMessage.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const FullScreenMessage = ({ children }: { children: ReactNode }) => {
+  return (
+    <View style={styles.container}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default FullScreenMessage;
