8fb500459bb871856dc2ad6b29cdece10dbe2824
Craig Mulligan <git@craigmulligan.com>
2024-02-04 12:14:19 -0800

    Add sentry

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import Map from "./components/Map";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+});
+
 function App() {
   return <Map />
 }
 
-export default App
+export default Sentry.wrap(App);
